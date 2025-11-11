5ac46e7ca100ba99cbec593fc1bfc33ca803f74e
Jappie Klooster <jappieklooster@hotmail.com>
2025-11-11 21:44:09 +0100

    init commit

---
+++ configuration.nix
@@ -0,0 +1,119 @@
+let
+  sources = import ./npins;
+
+in
+{
+  modulesPath,
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  inherit (lib.modules) mkAliasOptionModule;
+in
+{
+  imports = [
+    # The generator and hardware configuration
+    (modulesPath + "/installer/sd-card/sd-image-aarch64.nix")
+
+    #
+    # nixpkgs
+    #
+    {
+      nixpkgs = {
+        overlays = map import [
+          # ../overlays/agenix.nix
+        ];
+
+        # Set NIX_PATH and flake registry at the same time
+        # https://github.com/NixOS/nixpkgs/pull/254405
+        flake.source = sources.nixpkgs;
+      };
+
+      # TODO fuck is this? ask leana
+      # nix.package = pkgs.nix-monitored;
+
+      system.nixos.version = lib.substring 0 8 sources.nixpkgs.revision;
+    }
+
+
+    #
+    # Extern modules
+    #
+    # (sources.agenix + "/modules/age.nix")
+
+    (sources.nixos-hardware + "/raspberry-pi/4")
+
+    # #
+    # # Secrets
+    # #
+    # {
+    #   age.secrets = {
+    #     wpa_password.file = "${../secrets/wpa_password.age}";
+    #   };
+    # }
+  ];
+  environment.systemPackages = [
+    pkgs.man-pages
+    pkgs.man-pages-posix
+  ];
+
+  #
+  # Programs
+  #
+  programs = {
+    vim.enable = true;
+    vim.defaultEditor = true;
+
+    git.enable = true;
+  };
+
+  # Helps with kitty when ssh from remote
+  environment.enableAllTerminfo = true;
+  system.stateVersion = "25.05";
+
+  swapDevices = [
+    {
+      device = "/var/swapfile";
+      size = 1024; # MB
+    }
+  ];
+
+  # Related https://github.com/NixOS/nixpkgs/issues/154163#issuecomment-1350599022
+  #
+  # modprobe: FATAL: Module sun4i-drm not found in directory /nix/store/gvvwpdckzcr4iamp1iyrqw3nzb7bg6c4-linux-rpi-6.6.51-stable_20241008-modules/lib/modules/6.6.51
+  nixpkgs.overlays = [
+    (final: prev: {
+      makeModulesClosure = x: prev.makeModulesClosure (x // { allowMissing = true; });
+    })
+  ];
+
+  users.users.root.openssh.authorizedKeys.keys =
+    ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILYz4bDXj7t7NQ8QBYKKFBK8myYK6R8/8cybxlMUnIQn hi@jappie.me"]; # TODO
+
+  networking = {
+    networkmanager.enable = lib.mkForce false;
+
+    # To enable roaming https://wiki.archlinux.org/title/Wpa_supplicant#Roaming
+    wireless = {
+      enable = true;
+      userControlled.enable = true;
+      secretsFile = config.age.secrets.wpa_password.path;
+      scanOnLowSignal = false;
+      networks."jappie-hutje".psk = "jappiejappie";
+    };
+  };
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "prohibit-password";
+      PasswordAuthentication = false;
+    };
+  };
+
+
+  hardware.bluetooth.enable = true;
+
+
+}
---
+++ default.nix
@@ -0,0 +1,8 @@
+{
+  sources ? import ./npins,
+}:
+
+import (sources.nixpkgs + "/nixos/lib/eval-config.nix") {
+      system = "aarch64-linux";
+      modules = [ ./configuration.nix ];
+}
