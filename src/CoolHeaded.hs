c0e9bf282086f596c622a4cb8dd619c4a4cd41fc
Jappie Klooster <jappieklooster@hotmail.com>
2025-11-13 20:51:12 +0100

    okay this works for finding mac addresses

--- src/CoolHeaded.hs
+++ src/CoolHeaded.hs
@@ -6,32 +6,22 @@ where
 
 import Control.Concurrent (threadDelay)
 import Control.Monad (forM_)
-import Data.Int (Int16)
-import Data.Maybe (listToMaybe)
+import Data.List (nub)
+import Data.Maybe (listToMaybe, mapMaybe)
 import Data.Text (Text)
 import qualified Data.Text as T
 
 import DBus
 import DBus.Client
 
+newtype BleDevice = BleDevice { bleMac :: Text }
+    deriving (Eq, Ord, Show)
 
 bluezBusName :: BusName
 bluezBusName = busName_ "org.bluez"
 
 adapterPath :: ObjectPath
 adapterPath = objectPath_ "/org/bluez/hci0"
-
-data DecodeError
-    = TopLevelNotDict Type
-    | EntryKeyNotObjectPath Variant
-    | EntryValueNotDict ObjectPath Type
-    | IfaceNameNotText ObjectPath Variant
-    | PropsNotDict ObjectPath Type
-    | MissingProp ObjectPath Text
-    | BadPropType ObjectPath Text Type
-    deriving (Show)
-
-type Device = (Text, Int16)   -- (MAC, RSSI)
 
 main :: IO ()
 main = do
@@ -52,16 +42,12 @@ main = do
 
     putStrLn "Discovery stopped.\n"
 
-    (errs, devs) <- getDevicesWithRssi client
+    devices <- getBleDevices client
+    let uniqueDevices = nub devices
 
-    putStrLn "Devices found (MAC, RSSI):"
-    forM_ devs $ \(addr, rssi) ->
-        putStrLn $ T.unpack addr ++ "  RSSI=" ++ show rssi
-
-    putStrLn ""
-    putStrLn "Decode errors:"
-    forM_ errs $ \e ->
-        putStrLn ("  " ++ show e)
+    putStrLn "Devices found:"
+    forM_ uniqueDevices $ \(BleDevice mac) ->
+        putStrLn (T.unpack mac)
 
 powerOnAdapter :: Client -> IO ()
 powerOnAdapter client = do
@@ -98,96 +84,44 @@ powerOnAdapter client = do
                     }
             pure ()
 
-getDevicesWithRssi :: Client -> IO ([DecodeError], [Device])
-getDevicesWithRssi client = do
+getBleDevices :: Client -> IO [BleDevice]
+getBleDevices client = do
     reply <- call_ client $
         (methodCall "/" "org.freedesktop.DBus.ObjectManager" "GetManagedObjects")
             { methodCallDestination = Just bluezBusName }
 
     let body = methodReturnBody reply
-
     case body of
         [objectsVariant] ->
             case fromVariant objectsVariant :: Maybe Dictionary of
-                Nothing ->
-                    pure ([TopLevelNotDict (variantType objectsVariant)], [])
-                Just dict -> do
+                Nothing   -> pure []
+                Just dict ->
                     let entries = dictionaryItems dict
-                        results = map decodeTopEntry entries
-                        (errsLists, devLists) = unzip results
-                    pure (concat errsLists, concat devLists)
-        _ ->
-            pure ([TopLevelNotDict TypeVariant], [])  -- "impossible" shape but tagged
+                    in pure (mapMaybe decodeDevice entries)
+        _ -> pure []
 
--- Each top-level entry: key = object path, value = dict of interfaces
-decodeTopEntry
-    :: (Variant, Variant)
-    -> ([DecodeError], [Device])
-decodeTopEntry (pathVar, ifacesVar) =
-    case fromVariant pathVar :: Maybe ObjectPath of
-        Nothing ->
-            ([EntryKeyNotObjectPath pathVar], [])
-        Just path ->
-            case fromVariant ifacesVar :: Maybe Dictionary of
-                Nothing ->
-                    ([EntryValueNotDict path (variantType ifacesVar)], [])
-                Just ifacesDict ->
-                    let ifaceEntries = dictionaryItems ifacesDict
-                        results = map (decodeIface path) ifaceEntries
-                        (errsLists, devLists) = unzip results
-                    in (concat errsLists, concat devLists)
+decodeDevice :: (Variant, Variant) -> Maybe BleDevice
+decodeDevice (pathVar, ifacesVar) = do
+    _path <- fromVariant pathVar :: Maybe ObjectPath
+    ifacesDict <- fromVariant ifacesVar :: Maybe Dictionary
+    let ifaceEntries = dictionaryItems ifacesDict
+    propsVar <- lookupIface "org.bluez.Device1" ifaceEntries
+    propsDict <- fromVariant propsVar :: Maybe Dictionary
+    let props = dictionaryItems propsDict
+    addrVar <- lookupProp "Address" props
+    addr <- fromVariant =<< fromVariant addrVar :: Maybe Text
+    pure (BleDevice addr)
 
--- One interface entry on a path: key = interface name, value = dict of properties
-decodeIface
-    :: ObjectPath
-    -> (Variant, Variant)
-    -> ([DecodeError], [Device])
-decodeIface path (ifaceNameVar, propsVar) =
-    case fromVariant ifaceNameVar :: Maybe Text of
-        Nothing ->
-            ([IfaceNameNotText path ifaceNameVar], [])
-        Just ifaceName
-            | ifaceName /= "org.bluez.Device1" ->
-                ([], [])   -- not a device; ignore
-            | otherwise ->
-                case fromVariant propsVar :: Maybe Dictionary of
-                    Nothing ->
-                        ([PropsNotDict path (variantType propsVar)], [])
-                    Just propsDict ->
-                        decodeDeviceProps path (dictionaryItems propsDict)
+lookupIface :: Text -> [(Variant, Variant)] -> Maybe Variant
+lookupIface name =
+    fmap snd . listToMaybe . filter matches
+  where
+    matches (k, _) =
+        case fromVariant k :: Maybe Text of
+            Just n  -> n == name
+            Nothing -> False
 
--- Properties dict: key = property name (Text), value = Variant (wrapped)
-decodeDeviceProps
-    :: ObjectPath
-    -> [(Variant, Variant)]
-    -> ([DecodeError], [Device])
-decodeDeviceProps path props =
-    let mAddrVar = lookupProp "Address" props
-        mRssiVar = lookupProp "RSSI"   props
-    in case (mAddrVar, mRssiVar) of
-        (Nothing, _) ->
-            ([MissingProp path "Address"], [])
-        (_, Nothing) ->
-            ([MissingProp path "RSSI"], [])
-        (Just addrVar, Just rssiVar) ->
-            let mAddr :: Maybe Text
-                mAddr = fromVariant =<< fromVariant addrVar
-
-                mRssi :: Maybe Int16
-                mRssi = fromVariant =<< fromVariant rssiVar
-            in case (mAddr, mRssi) of
-                (Nothing, _) ->
-                    ([BadPropType path "Address" (variantType addrVar)], [])
-                (_, Nothing) ->
-                    ([BadPropType path "RSSI" (variantType rssiVar)], [])
-                (Just addr, Just rssi) ->
-                    ([], [(addr, rssi)])
-
--- Find a property by name in a{sv}
-lookupProp
-    :: Text
-    -> [(Variant, Variant)]
-    -> Maybe Variant
+lookupProp :: Text -> [(Variant, Variant)] -> Maybe Variant
 lookupProp name =
     fmap snd . listToMaybe . filter matches
   where
